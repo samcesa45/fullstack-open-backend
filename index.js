eb8ed0c2b62df6ae6d704414a2bc5b669912cc2d
samcesa45 <samcesa45@gmail.com>
2022-10-09 17:09:11 +0100

    minor heroku fixes

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.static('build'));
+app.use((0, morgan_1.default)('tiny'));
+(0, morgan_1.default)(':method :url :status :res[content-length] - :response-time ms');
 let persons = [
     {
         "id": 1,
@@ -27,27 +33,27 @@ let persons = [
         "number": "39-23-6423122"
     }
 ];
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (_req, res) => {
     const person = persons.map(person => person);
     if (person) {
-        response.json(person);
+        res.json(person);
     }
     else {
-        response.status(404).end();
+        res.status(404).end();
     }
 });
 // app.get('/info',(request: any,response: { send: (arg0: string) => void })=>{
 //     response.send(`<p>Phonebook has info for ${persons.length} people</p>`)
 //     // response.send(new Date().toISOString())
 // })
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
     const person = persons.find(person => person.id === id);
     if (person) {
-        response.json(person);
+        res.json(person);
     }
     else {
-        response.status(404).end();
+        res.status(404).end();
     }
 });
 app.delete('/api/persons/:id', (request, response) => {
@@ -59,20 +65,25 @@ const generateId = () => {
     const maxId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) : 0;
     return maxId + 1;
 };
-app.post('/api/persons', (request, response) => {
-    const body = request.body;
-    // if(!body.name){
-    //     return response.status(400).json({error:'no content'})
-    // }
-    // console.log(body.name);
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    if (!body.name || !body.number) {
+        res.status(404).json({ error: 'no content' });
+    }
     const person = {
         id: generateId(),
         name: body.name,
         number: body.number
     };
+    console.log(body.name, person.name);
+    // const personName = persons.find(person=> person)?.name
+    // if(personName?.match(body.name)){
+    //  return res.status(400).json({error:'name or number must be unique'})  
+    // }
     persons = persons.concat(person);
-    response.json(person);
+    return res.json(person);
 });
-const PORT = 5000;
-app.listen(PORT);
-console.log(`Server running on PORT ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
