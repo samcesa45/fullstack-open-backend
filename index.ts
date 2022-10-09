ae631b6d39b47da6aaacaf31f4cb88b966a6f689
samcesa45 <samcesa45@gmail.com>
2022-10-09 10:21:40 +0100

    added env var

--- index.ts
+++ index.ts
@@ -122,6 +122,6 @@ app.post('/api/persons',(req:Request<{},{},{name:string,number:string},{}>, res:
     
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT)
 console.log(`Server running on PORT ${PORT}`)
